8bc482737ceb4579d1df2185c6d5eb692b16bc07
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-02 22:51:14 -0500

    se cambia el tipo de objeto

--- src/main/java/com/seguros/app/polizas/models/repository/ClienteRepository.java
+++ src/main/java/com/seguros/app/polizas/models/repository/ClienteRepository.java
@@ -3,9 +3,9 @@ package com.seguros.app.polizas.models.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import ch.qos.logback.core.net.server.Client;
+import com.seguros.app.polizas.models.entity.Cliente;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Client, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 	
 }
