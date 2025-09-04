68e5c97d867566be42cca061e8a005f596216486
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-09-03 21:37:54 -0500

    se agrega configuración de los CORS

--- src/main/java/com/seguros/app/polizas/GestionPolizasApplication.java
+++ src/main/java/com/seguros/app/polizas/GestionPolizasApplication.java
@@ -2,6 +2,9 @@ package com.seguros.app.polizas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GestionPolizasApplication {
@@ -9,5 +12,21 @@ public class GestionPolizasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestionPolizasApplication.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/api/**")                       // URLs que permiten CORS
+	                    .allowedOrigins("http://localhost:4200")     // Orígenes permitidos (Angular)
+	                    .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+	                    .allowedHeaders("*")                          // Cabeceras permitidas
+	                    .allowCredentials(true)                       // Permite enviar cookies/autenticación
+	                    .maxAge(3600);                                // Tiempo que dura la configuración en cache
+	        }
+	    };
+	}
+
 
 }
